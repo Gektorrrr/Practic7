2317ceb179cf36d0a382e5693ca3958d798584ee
Gektorrrr <zenaslikov@gmail.com>
2021-11-08 21:41:36 +0200

    first commit

---
+++ table.php
@@ -0,0 +1,23 @@
+<?php
+if (!file_exists('database/users.csv')) {
+    file_put_contents('database/users.csv', '');
+}
+ 
+$file = fopen('database/users.csv', 'r');
+
+$str = file_get_contents('database/users.csv');
+$users = explode("\n", $str);
+?>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<div class="container card-panel">
+       <?php
+
+        for($i = 0; $i < count($users) - 1; $i++) {
+            echo str_replace(', ', " | ", $users[$i]) . "<br>";
+            echo "<hr>";
+        }
+       ?> 
+       <a class="btn" href="adduser.php">return back</a>
+</div>
